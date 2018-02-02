8cbb8a1f8727998a6a8035237ebfc1abb831723a
Jeff Langr <Jeff@Langrsoft.com>
2018-02-02 06:27:49 -0700

    Update AnAuthorNameNormalizer.java

--- java/src/test/java/util/AnAuthorNameNormalizer.java
+++ java/src/test/java/util/AnAuthorNameNormalizer.java
@@ -8,15 +8,6 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-// 1. Un-ignore the next commented-out test method.
-// 2. Run JUnit against *all* tests in the project.
-// 3. Did the current test fail? If not: You built too much code in a prior step. Undo work for prior tests and try again.
-// 4. Make sure you are clear on why the test failed.
-// 5. Write only enough code to make that failing test pass (and not break any other tests).
-//    Did you write too much? Is there a simple way to get that test to pass???
-// 6. If there is a commented-out assertion, uncomment it. It should fail. If not, return to step 5.
-// 7. Repeat prior steps until no more test methods exist.
-
 public class AnAuthorNameNormalizer {
     private AuthorNameNormalizer normalizer =
             new AuthorNameNormalizer();
