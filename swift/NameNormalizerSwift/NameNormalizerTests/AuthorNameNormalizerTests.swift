48a3a1708d55c5ed0cdd1fc9637770446c6d6b46
Jon Reid <jon@qualitycoding.org>
2018-02-10 12:38:58 -0800

    Swift: use a free function instead of member function

--- swift/NameNormalizerSwift/NameNormalizerTests/AuthorNameNormalizerTests.swift
+++ swift/NameNormalizerSwift/NameNormalizerTests/AuthorNameNormalizerTests.swift
@@ -2,51 +2,40 @@
 import XCTest
 
 class AuthorNameNormalizerTests: XCTestCase {
-    var normalizer: AuthorNameNormalizer!
-
-    override func setUp() {
-        super.setUp()
-        normalizer = AuthorNameNormalizer()
-    }
-
-    override func tearDown() {
-        normalizer = nil
-        super.tearDown()
-    }
 
     func DISABLED_testReturnsEmptyStringWhenEmpty() {
-        XCTAssertEqual(try normalizer.normalize(""), "")
+        XCTAssertEqual(try normalize(name: ""), "")
     }
 
     func DISABLED_testReturnsSingleWordName() {
-        XCTAssertEqual(try normalizer.normalize("Plato"), "Plato")
+        XCTAssertEqual(try normalize(name: "Plato"), "Plato")
     }
 
     func DISABLED_testSwapsFirstAndLastNames() {
-        XCTAssertEqual(try normalizer.normalize("Haruki Murakami"), "Murakami, Haruki")
+        XCTAssertEqual(try normalize(name: "Haruki Murakami"), "Murakami, Haruki")
     }
 
     func DISABLED_testTrimsLeadingAndTrailingWhitespace() {
-        XCTAssertEqual(try normalizer.normalize("  Big Boi   "), "Boi, Big")
+        XCTAssertEqual(try normalize(name: "  Big Boi   "), "Boi, Big")
     }
 
     func DISABLED_testInitializesMiddleName() {
-        XCTAssertEqual(try normalizer.normalize("Henry David Thoreau"), "Thoreau, Henry D.")
+        XCTAssertEqual(try normalize(name: "Henry David Thoreau"), "Thoreau, Henry D.")
     }
 
     func DISABLED_testDoesNotInitializeOneLetterMiddleName() {
-        XCTAssertEqual(try normalizer.normalize("Harry S Truman"), "Truman, Harry S")
+        XCTAssertEqual(try normalize(name: "Harry S Truman"), "Truman, Harry S")
     }
 
     func DISABLED_testInitializesEachOfMultipleMiddleNames() {
-        XCTAssertEqual(try normalizer.normalize("Julia Scarlett Elizabeth Louis-Dreyfus"), "Louis-Dreyfus, Julia S. E.")
+        XCTAssertEqual(try normalize(name: "Julia Scarlett Elizabeth Louis-Dreyfus"), "Louis-Dreyfus, Julia S. E.")
     }
 
     func DISABLED_testAppendsSuffixesToEnd() {
-        XCTAssertEqual(try normalizer.normalize("Martin Luther King, Jr."), "King, Martin L., Jr.")
+        XCTAssertEqual(try normalize(name: "Martin Luther King, Jr."), "King, Martin L., Jr.")
     }
 
     func DISABLED_testThrowsWhenNameContainsTwoCommas() {
-        XCTAssertThrowsError(try normalizer.normalize("Thurston, Howell, III"))
+        XCTAssertThrowsError(try normalize(name: "Thurston, Howell, III"))
     }
 }
