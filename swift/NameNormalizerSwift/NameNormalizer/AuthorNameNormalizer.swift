73fbb3048fd022af064279b33a6b3ec23b26efff
Jon Reid <jon@qualitycoding.org>
2018-02-11 13:57:39 -0800

    Swift: Pass testSwapsFirstAndLastNames

--- swift/NameNormalizerSwift/NameNormalizer/AuthorNameNormalizer.swift
+++ swift/NameNormalizerSwift/NameNormalizer/AuthorNameNormalizer.swift
@@ -1,3 +1,7 @@
 public func normalize(name: String) throws -> String {
+    let parts = name.split(separator: " ")
+    if parts.count > 1 {
+        return "\(parts.last!), \(parts.first!)"
+    }
     return name
 }
