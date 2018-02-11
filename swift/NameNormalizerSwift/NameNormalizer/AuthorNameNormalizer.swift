51137079969792a595df1fe59af16371b0e8bae0
Jon Reid <jon@qualitycoding.org>
2018-02-11 14:17:15 -0800

    Swift: Pass testInitializesMiddleName with expedient but ugly code

--- swift/NameNormalizerSwift/NameNormalizer/AuthorNameNormalizer.swift
+++ swift/NameNormalizerSwift/NameNormalizer/AuthorNameNormalizer.swift
@@ -1,5 +1,9 @@
 public func normalize(name: String) throws -> String {
     let parts = name.split(separator: " ")
+    if parts.count > 2 {
+        let middleInitial = parts[1].first!
+        return "\(parts.last!), \(parts.first!) \(middleInitial)."
+    }
     if parts.count > 1 {
         return "\(parts.last!), \(parts.first!)"
     }
