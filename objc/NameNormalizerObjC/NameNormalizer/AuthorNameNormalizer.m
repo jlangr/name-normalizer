cfc155e3a08a9f508bd944037cfdc4bfad7e5f53
Jon Reid <jon@qualitycoding.org>
2018-02-16 01:37:51 -0800

    ObjC: Refactor so `return name` comes first

--- objc/NameNormalizerObjC/NameNormalizer/AuthorNameNormalizer.m
+++ objc/NameNormalizerObjC/NameNormalizer/AuthorNameNormalizer.m
@@ -6,14 +6,14 @@
 {
     NSString *trimmed = [name stringByTrimmingCharactersInSet:NSCharacterSet.whitespaceCharacterSet];
     NSArray<NSString *> *parts = [trimmed componentsSeparatedByString:@" "];
+    if (parts.count < 2)
+        return name;
     if (parts.count > 2)
     {
         NSString *middleInitial = [parts[1] substringToIndex:1];
         return [NSString stringWithFormat:@"%@, %@ %@.", parts.lastObject, parts.firstObject, middleInitial];
     }
-    if (parts.count > 1)
-        return [NSString stringWithFormat:@"%@, %@", parts.lastObject, parts.firstObject];
-    return name;
+    return [NSString stringWithFormat:@"%@, %@", parts.lastObject, parts.firstObject];
 }
 
 @end
