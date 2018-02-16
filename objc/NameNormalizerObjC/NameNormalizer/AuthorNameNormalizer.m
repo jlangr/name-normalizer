96ca950e6c1810e41c865472ca912db1d3f289b8
Jon Reid <jon@qualitycoding.org>
2018-02-16 01:29:58 -0800

    Objc: Pass testInitializesMiddleName with expedient but ugly code

--- objc/NameNormalizerObjC/NameNormalizer/AuthorNameNormalizer.m
+++ objc/NameNormalizerObjC/NameNormalizer/AuthorNameNormalizer.m
@@ -6,6 +6,11 @@
 {
     NSString *trimmed = [name stringByTrimmingCharactersInSet:NSCharacterSet.whitespaceCharacterSet];
     NSArray<NSString *> *parts = [trimmed componentsSeparatedByString:@" "];
+    if (parts.count > 2)
+    {
+        NSString *middleInitial = [parts[1] substringToIndex:1];
+        return [NSString stringWithFormat:@"%@, %@ %@.", parts.lastObject, parts.firstObject, middleInitial];
+    }
     if (parts.count > 1)
         return [NSString stringWithFormat:@"%@, %@", parts.lastObject, parts.firstObject];
     return name;
