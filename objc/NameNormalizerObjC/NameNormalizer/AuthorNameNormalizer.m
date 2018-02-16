ea4f934531050b173eb413dd04f2f1d7fab0b67e
Jon Reid <jon@qualitycoding.org>
2018-02-16 01:41:08 -0800

    ObjC: Refactor eliminate duplicate returns

--- objc/NameNormalizerObjC/NameNormalizer/AuthorNameNormalizer.m
+++ objc/NameNormalizerObjC/NameNormalizer/AuthorNameNormalizer.m
@@ -10,10 +10,7 @@
         return name;
     NSString *middleInitial = @"";
     if (parts.count > 2)
-    {
         middleInitial = [NSString stringWithFormat:@" %@.", [parts[1] substringToIndex:1]];
-        return [NSString stringWithFormat:@"%@, %@%@", parts.lastObject, parts.firstObject, middleInitial];
-    }
     return [NSString stringWithFormat:@"%@, %@%@", parts.lastObject, parts.firstObject, middleInitial];
 }
 
