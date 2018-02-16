46d9010d00e7a6d62dc2554e3a24b2deab8a1ee3
Jon Reid <jon@qualitycoding.org>
2018-02-16 01:18:13 -0800

    ObjC: Pass testSwapsFirstAndLastNames

--- objc/NameNormalizerObjC/NameNormalizer/AuthorNameNormalizer.m
+++ objc/NameNormalizerObjC/NameNormalizer/AuthorNameNormalizer.m
@@ -4,6 +4,9 @@
 
 - (NSString *)normalize:(NSString *)name
 {
+    NSArray<NSString *> *parts = [name componentsSeparatedByString:@" "];
+    if (parts.count > 1)
+        return [NSString stringWithFormat:@"%@, %@", parts.lastObject, parts.firstObject];
     return name;
 }
 
