5d67add35bdf25e4652488edcffa84c6a20365cb
Jon Reid <jon@qualitycoding.org>
2019-11-01 14:26:52 -0700

    Kotlin: Refactor extracting method parts(name:)

--- kotlin/src/main/AuthorNameNormalizer.kt
+++ kotlin/src/main/AuthorNameNormalizer.kt
@@ -1,7 +1,9 @@
 class AuthorNameNormalizer() {
     fun normalize(name: String): String {
-        val nameParts = name.trim().split(" ")
+        val nameParts = parts(name)
         if (nameParts.count() == 1) return name
         return "${nameParts.last()}, ${nameParts.first()}"
     }
+
+    private fun parts(name: String) = name.trim().split(" ")
 }
