4c24bed59159461f608aee05595bbdb1580831a6
Jon Reid <jon@qualitycoding.org>
2019-11-01 14:25:59 -0700

    Kotlin: Pass `trims leading and trailing whitespace`

--- kotlin/src/main/AuthorNameNormalizer.kt
+++ kotlin/src/main/AuthorNameNormalizer.kt
@@ -1,6 +1,6 @@
 class AuthorNameNormalizer() {
     fun normalize(name: String): String {
-        val nameParts = name.split(" ")
+        val nameParts = name.trim().split(" ")
         if (nameParts.count() == 1) return name
         return "${nameParts.last()}, ${nameParts.first()}"
     }
