3fbfe0d4874d6091159e5f57d4cd3b5099f156cb
Jon Reid <jon@qualitycoding.org>
2019-11-01 14:32:11 -0700

    Kotlin: Pass `initializes middle name`

--- kotlin/src/main/AuthorNameNormalizer.kt
+++ kotlin/src/main/AuthorNameNormalizer.kt
@@ -2,10 +2,16 @@ class AuthorNameNormalizer() {
     fun normalize(name: String): String {
         val nameParts = parts(name)
         if (isMononym(nameParts)) return name
-        return "${nameParts.last()}, ${nameParts.first()}"
+        return "${nameParts.last()}, ${nameParts.first()}${middleInitial(nameParts)}"
     }
 
     private fun parts(name: String) = name.trim().split(" ")
 
     private fun isMononym(nameParts: List<String>) = nameParts.count() == 1
+
+    private fun middleInitial(nameParts: List<String>): String {
+        if (nameParts.count() <= 2) return ""
+        val middleName = nameParts[1]
+        return " " + middleName.first() + "."
+    }
 }
