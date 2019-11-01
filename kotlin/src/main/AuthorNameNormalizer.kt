4d848a9f540b78ede32d4912cb9b46a082c96be4
Jon Reid <jon@qualitycoding.org>
2019-11-01 15:08:44 -0700

    Kotlin: Refactor renaming method to middleInitials

--- kotlin/src/main/AuthorNameNormalizer.kt
+++ kotlin/src/main/AuthorNameNormalizer.kt
@@ -2,14 +2,14 @@ class AuthorNameNormalizer() {
     fun normalize(name: String): String {
         val nameParts = parts(name)
         if (isMononym(nameParts)) return name
-        return "${nameParts.last()}, ${nameParts.first()}${middleInitial(nameParts)}"
+        return "${nameParts.last()}, ${nameParts.first()}${middleInitials(nameParts)}"
     }
 
     private fun parts(name: String) = name.trim().split(" ")
 
     private fun isMononym(nameParts: List<String>) = nameParts.count() == 1
 
-    private fun middleInitial(nameParts: List<String>): String {
+    private fun middleInitials(nameParts: List<String>): String {
         val middleNames = nameParts.drop(1).dropLast(1)
         return buildString {
             middleNames.forEach { append(initialized(it)) }
