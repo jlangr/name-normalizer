8eb05e27af137d319470dfba6fdffc849de0300c
Kerry Buckley <kerryjbuckley@gmail.com>
2020-02-09 21:55:37 +0000

    Pull success tuple up to top level function

--- elixir/lib/name_normalizer.ex
+++ elixir/lib/name_normalizer.ex
@@ -1,13 +1,13 @@
 defmodule NameNormalizer do
-  def normalize(name), do: name |> String.split() |> handle_parts()
+  def normalize(name), do: {:ok, name |> String.split() |> handle_parts()}
 
-  defp handle_parts([]), do: {:ok, ""}
-  defp handle_parts([single_name]), do: {:ok, single_name}
-  defp handle_parts([first_name, last_name]), do: {:ok, "#{last_name}, #{first_name}"}
+  defp handle_parts([]), do: ""
+  defp handle_parts([single_name]), do: single_name
+  defp handle_parts([first_name, last_name]), do: "#{last_name}, #{first_name}"
 
   defp handle_parts([first_name | other_names]) do
     {middle_names, last_name} = Enum.split(other_names, -1)
-    {:ok, Enum.join(["#{last_name},", first_name, initials(middle_names)], " ")}
+    Enum.join(["#{last_name},", first_name, initials(middle_names)], " ")
   end
 
   defp initials(middle_names) do
