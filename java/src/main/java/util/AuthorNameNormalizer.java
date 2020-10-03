7bdeeeafb017081ae53d88d0d3c72c6b3c4f4d34
Jeff L <jeff@langrsoft.com>
2020-10-03 10:58:26 -0600

    introduced abstractions to clarify first name, last name, and the conditional isMononym

--- java/src/main/java/util/AuthorNameNormalizer.java
+++ java/src/main/java/util/AuthorNameNormalizer.java
@@ -3,8 +3,20 @@ package util;
 public class AuthorNameNormalizer {
     public String normalize(String name) {
         var parts = name.split(" ");
-        if (parts.length == 1)
+        if (isMononym(parts))
             return name;
-        return parts[1] + ", " + parts[0];
+        return last(parts) + ", " + first(parts);
+    }
+
+    private boolean isMononym(String[] parts) {
+        return parts.length == 1;
+    }
+
+    private String first(String[] parts) {
+        return parts[0];
+    }
+
+    private String last(String[] parts) {
+        return parts[1];
     }
 }
