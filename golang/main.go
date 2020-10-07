4bb9715244114761ea05761c94865f95f388fc4d
Akeshihiro <akeshihiro@gmx.net>
2020-10-07 22:09:22 +0200

    Format code by gofmt

--- golang/main.go
+++ golang/main.go
@@ -1,105 +1,105 @@
 package authornames
 
 import (
-    "fmt"
-    "strings"
+	"fmt"
+	"strings"
 )
 
 type authorName struct {
-    firstName   string
-    middleNames []string
-    lastName    string
-    suffix      string
+	firstName   string
+	middleNames []string
+	lastName    string
+	suffix      string
 }
 
 func (n authorName) getMiddleNamesAsInitializedString() string {
-    if n.middleNames == nil || len(n.middleNames) == 0 {
-        return ""
-    }
+	if n.middleNames == nil || len(n.middleNames) == 0 {
+		return ""
+	}
 
-    result := ""
-    for _, v := range n.middleNames {
-        if len(v) == 1 {
-            result += v + " "
-        } else {
-            result += string(v[0]) + ". "
-        }
-    }
+	result := ""
+	for _, v := range n.middleNames {
+		if len(v) == 1 {
+			result += v + " "
+		} else {
+			result += string(v[0]) + ". "
+		}
+	}
 
-    return strings.TrimSpace(result)
+	return strings.TrimSpace(result)
 }
 
 func (n authorName) String() string {
-    result := n.lastName + ", " + n.firstName
+	result := n.lastName + ", " + n.firstName
 
-    if n.middleNames != nil && len(n.middleNames) > 0 {
-        result += " " + n.getMiddleNamesAsInitializedString()
-    }
-    if n.suffix != "" {
-        result += ", " + n.suffix
-    }
+	if n.middleNames != nil && len(n.middleNames) > 0 {
+		result += " " + n.getMiddleNamesAsInitializedString()
+	}
+	if n.suffix != "" {
+		result += ", " + n.suffix
+	}
 
-    return result
+	return result
 }
 
 func Normalize(s string) (string, error) {
-    if strings.Count(s, ",") > 1 {
-        return "", fmt.Errorf("there are too much commas in '%v'", s)
-    }
+	if strings.Count(s, ",") > 1 {
+		return "", fmt.Errorf("there are too much commas in '%v'", s)
+	}
 
-    s = strings.TrimSpace(s)
+	s = strings.TrimSpace(s)
 
-    if isSingleWordName(s) {
-        return s, nil
-    }
+	if isSingleWordName(s) {
+		return s, nil
+	}
 
-    name := parseAuthorName(s)
+	name := parseAuthorName(s)
 
-    return name.String(), nil
+	return name.String(), nil
 }
 
 func parseAuthorName(s string) authorName {
-    suffix := ""
-    if strings.Contains(s, ",") {
-        parts := strings.Split(s, ",")
-        suffix = strings.TrimSpace(parts[1])
-        s = strings.TrimSpace(parts[0])
-    }
+	suffix := ""
+	if strings.Contains(s, ",") {
+		parts := strings.Split(s, ",")
+		suffix = strings.TrimSpace(parts[1])
+		s = strings.TrimSpace(parts[0])
+	}
 
-    parts := strings.Split(s, " ")
+	parts := strings.Split(s, " ")
 
-    return authorName{
-        firstName:   determineFirstName(parts),
-        middleNames: determineMiddleNames(parts),
-        lastName:    determineLastName(parts),
-        suffix:      suffix,
-    }
+	return authorName{
+		firstName:   determineFirstName(parts),
+		middleNames: determineMiddleNames(parts),
+		lastName:    determineLastName(parts),
+		suffix:      suffix,
+	}
 }
 
 func determineMiddleNames(parts []string) []string {
-    if parts == nil || len(parts) < 3 {
-        return nil
-    }
+	if parts == nil || len(parts) < 3 {
+		return nil
+	}
 
-    return parts[1 : len(parts)-1]
+	return parts[1 : len(parts)-1]
 }
 
 func determineLastName(parts []string) string {
-    if parts == nil || len(parts) == 0 {
-        return ""
-    }
+	if parts == nil || len(parts) == 0 {
+		return ""
+	}
 
-    return parts[len(parts)-1]
+	return parts[len(parts)-1]
 }
 
 func determineFirstName(parts []string) string {
-    if parts == nil || len(parts) == 0 {
-        return ""
-    }
+	if parts == nil || len(parts) == 0 {
+		return ""
+	}
 
-    return parts[0]
+	return parts[0]
 }
 
 func isSingleWordName(s string) bool {
-    return !strings.Contains(s, " ")
+	return !strings.Contains(s, " ")
 }
--- golang/main_test.go
+++ golang/main_test.go
@@ -1,8 +1,8 @@
 package authornames_test
 
 import (
-    authornames "github.com/jlangr/name-normalizer/golang"
-    "testing"
+	authornames "github.com/jlangr/name-normalizer/golang"
+	"testing"
 )
 
 // To enable a test just change the first letter of the test function to upper case,
@@ -11,96 +11,96 @@ import (
 // To run the tests just run 'go test' in the terminal.
 
 func Test_ShouldReturnEmptyString_WhenNameIsEmpty(t *testing.T) {
-    expected := ""
-    actual, err := authornames.Normalize("")
-    if err != nil {
-        t.Fatal(err)
-    }
-    if expected != actual {
-        t.Fatalf("expected: '%v', actual: '%v'", expected, actual)
-    }
+	expected := ""
+	actual, err := authornames.Normalize("")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if expected != actual {
+		t.Fatalf("expected: '%v', actual: '%v'", expected, actual)
+	}
 }
 
 func Test_ShouldReturnSingleWordName(t *testing.T) {
-    expected := "Plato"
-    actual, err := authornames.Normalize("Plato")
-    if err != nil {
-        t.Fatal(err)
-    }
-    if expected != actual {
-        t.Fatalf("expected: '%v', actual: '%v'", expected, actual)
-    }
+	expected := "Plato"
+	actual, err := authornames.Normalize("Plato")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if expected != actual {
+		t.Fatalf("expected: '%v', actual: '%v'", expected, actual)
+	}
 }
 
 func Test_ShouldSwapFirstAndLastNames(t *testing.T) {
-    expected := "Murakami, Haruki"
-    actual, err := authornames.Normalize("Haruki Murakami")
-    if err != nil {
-        t.Fatal(err)
-    }
-    if expected != actual {
-        t.Fatalf("expected: '%v', actual: '%v'", expected, actual)
-    }
+	expected := "Murakami, Haruki"
+	actual, err := authornames.Normalize("Haruki Murakami")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if expected != actual {
+		t.Fatalf("expected: '%v', actual: '%v'", expected, actual)
+	}
 }
 
 func Test_ShouldTrimLeadingAndTrailingWhitespaces(t *testing.T) {
-    expected := "Boi, Big"
-    actual, err := authornames.Normalize("  Big Boi   ")
-    if err != nil {
-        t.Fatal(err)
-    }
-    if expected != actual {
-        t.Fatalf("expected: '%v', actual: '%v'", expected, actual)
-    }
+	expected := "Boi, Big"
+	actual, err := authornames.Normalize("  Big Boi   ")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if expected != actual {
+		t.Fatalf("expected: '%v', actual: '%v'", expected, actual)
+	}
 }
 
 func Test_ShouldInitializeMiddleName(t *testing.T) {
-    expected := "Thoreau, Henry D."
-    actual, err := authornames.Normalize("Henry David Thoreau")
-    if err != nil {
-        t.Fatal(err)
-    }
-    if expected != actual {
-        t.Fatalf("expected: '%v', actual: '%v'", expected, actual)
-    }
+	expected := "Thoreau, Henry D."
+	actual, err := authornames.Normalize("Henry David Thoreau")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if expected != actual {
+		t.Fatalf("expected: '%v', actual: '%v'", expected, actual)
+	}
 }
 
 func Test_ShouldNotInitializeMiddleName_WhenMiddleNameIsOneLetter(t *testing.T) {
-    expected := "Truman, Harry S"
-    actual, err := authornames.Normalize("Harry S Truman")
-    if err != nil {
-        t.Fatal(err)
-    }
-    if expected != actual {
-        t.Fatalf("expected: '%v', actual: '%v'", expected, actual)
-    }
+	expected := "Truman, Harry S"
+	actual, err := authornames.Normalize("Harry S Truman")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if expected != actual {
+		t.Fatalf("expected: '%v', actual: '%v'", expected, actual)
+	}
 }
 
 func Test_ShouldInitializeEachOfMultipleMiddleNames(t *testing.T) {
-    expected := "Louis-Dreyfus, Julia S. E."
-    actual, err := authornames.Normalize("Julia Scarlett Elizabeth Louis-Dreyfus")
-    if err != nil {
-        t.Fatal(err)
-    }
-    if expected != actual {
-        t.Fatalf("expected: '%v', actual: '%v'", expected, actual)
-    }
+	expected := "Louis-Dreyfus, Julia S. E."
+	actual, err := authornames.Normalize("Julia Scarlett Elizabeth Louis-Dreyfus")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if expected != actual {
+		t.Fatalf("expected: '%v', actual: '%v'", expected, actual)
+	}
 }
 
 func Test_ShouldAppendSuffixesToEnd(t *testing.T) {
-    expected := "King, Martin L., Jr."
-    actual, err := authornames.Normalize("Martin Luther King, Jr.")
-    if err != nil {
-        t.Fatal(err)
-    }
-    if expected != actual {
-        t.Fatalf("expected: '%v', actual: '%v'", expected, actual)
-    }
+	expected := "King, Martin L., Jr."
+	actual, err := authornames.Normalize("Martin Luther King, Jr.")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if expected != actual {
+		t.Fatalf("expected: '%v', actual: '%v'", expected, actual)
+	}
 }
 
 func Test_ShouldReturnAnError_WhenNameContainsTwoCommas(t *testing.T) {
-    _, err := authornames.Normalize("Thurston, Howell, III")
-    if err == nil {
-        t.Fatal("expected an error, but nothing went wrong ...")
-    }
+	_, err := authornames.Normalize("Thurston, Howell, III")
+	if err == nil {
+		t.Fatal("expected an error, but nothing went wrong ...")
+	}
 }
