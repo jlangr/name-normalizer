e9c31c35701aa515c4017c5845003dfff96ef4f1
Jeff L <jeff@langrsoft.com>
2020-12-23 12:14:39 -0700

    moved C# solution into all-solutions

--- csharp/NameNormalizer/NameNormalizer/AnAuthorNameNormalizer.cs
+++ csharp/NameNormalizer/NameNormalizer/AnAuthorNameNormalizer.cs
@@ -13,55 +13,55 @@ namespace Tests
             normalizer = new AuthorNameNormalizer();
         }
 
-        [Fact(Skip="start here! Remove the skip argument.")]
+        [Fact]
         public void ReturnsEmptyStringWhenEmpty()
         {
             normalizer.Normalize("").Should().Be("");
         }
 
-        [Fact(Skip="remove when ready")]
+        [Fact]
         public void ReturnsSingleWordName()
         {
             normalizer.Normalize("Plato").Should().Be("Plato");
         }
 
-        [Fact(Skip="remove when ready")]
+        [Fact]
         public void ReturnsLastFirstWhenFirstLastProvided()
         {
             normalizer.Normalize("Haruki Murakami").Should().Be("Murakami, Haruki");
         }
 
-        [Fact(Skip="remove when ready")]
+        [Fact]
         public void TrimsWhitespace()
         {
             normalizer.Normalize("   Big Boi    ").Should().Be("Boi, Big");
         }
 
-        [Fact(Skip="remove when ready")]
+        [Fact]
         public void InitializesMiddleName()
         {
             normalizer.Normalize("Henry David Thoreau").Should().Be("Thoreau, Henry D.");
         }
 
-        [Fact(Skip="remove when ready")]
+        [Fact]
         public void DoesNotInitializeOneLetterMiddleName()
         {
             normalizer.Normalize("Harry S Truman").Should().Be("Truman, Harry S");
         }
 
-        [Fact(Skip="remove when ready")]
+        [Fact]
         public void InitializesEachOfMultipleMiddleNames()
         {
             normalizer.Normalize("Julia Scarlett Elizabeth Louis-Dreyfus").Should().Be("Louis-Dreyfus, Julia S. E.");
         }
 
-        [Fact(Skip="remove when ready")]
+        [Fact]
         public void AppendsSuffixesToEnd()
         {
             normalizer.Normalize("Martin Luther King, Jr.").Should().Be("King, Martin L., Jr.");
         }
 
-        [Fact(Skip="remove when ready")]
+        [Fact]
         public void ThrowsWhenNameContainsTwoCommas()
         {
             Action action = () => normalizer.Normalize("Thurston, Howell, III");
--- csharp/NameNormalizer/Prod/AuthorNameNormalizer.cs
+++ csharp/NameNormalizer/Prod/AuthorNameNormalizer.cs
@@ -6,9 +6,56 @@ namespace Prod
 {
     public class AuthorNameNormalizer
     {
-        public string Normalize(string v)
+        public string Normalize(string name)
         {
-            throw new NotImplementedException();
+            ThrowWhenContainsTooManyCommas(name);
+            var parts = Suffixless(name).Trim().Split(' ');
+            if (parts.Length == 1)
+                return name;
+            return $"{LastName(parts)}, {FirstName(parts)}{MiddleInitials(parts)}{Suffix(name)}";
         }
+
+        private void ThrowWhenContainsTooManyCommas(string name)
+        {
+            if (name.Count(c => c == ',') > 1)
+                throw new ArgumentException();
+        }
+
+        private string Suffix(string name)
+        {
+            var i = name.IndexOf(',');
+            if (i == -1) return "";
+            return name.Substring(i);
+        }
+
+        private string Suffixless(string name)
+        {
+            var i = name.IndexOf(',');
+            if (i == -1) return name;
+            return name.Substring(0, i);
+        }
+
+        private string MiddleInitials(string[] parts)
+        {
+            var middleNames = parts.Skip(1).Take(parts.Length - 2);
+            return string.Join("", middleNames.Select(Initial));
+        }
+
+        private string Initial(string name)
+        {
+            var period = (name.Length > 1) ? "." : "";
+            return $" {name[0]}{period}";
+        }
+
+        private string FirstName(string[] parts)
+        {
+            return parts[0];
+        }
+
+        private string LastName(string[] parts)
+        {
+            return parts[parts.Length - 1];
+        }
+        
     }
 }
