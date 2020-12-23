6f5c6007ff1e3a8583541c32d4b4eb82ea0eb6b2
Jeff L <jeff@langrsoft.com>
2020-12-23 13:31:01 -0700

    uncomment all tests

--- clojure/test/name_normalizer/core_test.clj
+++ clojure/test/name_normalizer/core_test.clj
@@ -3,21 +3,21 @@
             [name-normalizer.core :refer :all]))
 
 (deftest a-name-normalizer
-  #_(testing "returns empty string when passed an empty string"
+  (testing "returns empty string when passed an empty string"
     (is (= "" (normalize ""))))
-  #_(testing "returns single word name"
+  (testing "returns single word name"
     (is (= "Plato" (normalize "Plato"))))
-  #_(testing "swaps first and last names"
+  (testing "swaps first and last names"
     (is (= "Murakami, Haruki" (normalize "Haruki Murakami"))))
-  #_(testing "trims leading and trailing whitespace"
+  (testing "trims leading and trailing whitespace"
     (is (= "Boi, Big" (normalize "   Big Boi   "))))
-  #_(testing "initializes middle name"
+  (testing "initializes middle name"
     (is (= "Thoreau, Henry D." (normalize "Henry David Thoreau"))))
-  #_(testing "does not initialize one letter middle name"
+  (testing "does not initialize one letter middle name"
     (is (= "Truman, Harry S" (normalize "Harry S Truman"))))
-  #_(testing "initializes each of multiple middle names"
+  (testing "initializes each of multiple middle names"
     (is (= "Louis-Dreyfus, Julia S. E." (normalize "Julia Scarlett Elizabeth Louis-Dreyfus"))))
-  #_(testing "appends suffixes to end"
+  (testing "appends suffixes to end"
     (is (= "King, Martin L., Jr." (normalize "Martin Luther King, Jr."))))
-  #_(testing "throws when name contains two commas"
+  (testing "throws when name contains two commas"
     (is (thrown? IllegalArgumentException (normalize "Thurston, Howell, III")))))
