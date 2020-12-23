4424df9b44cd8d8776b3dac2491c1fff91d7c2f7
Jeff L <jeff@langrsoft.com>
2020-12-23 13:57:02 -0700

    use subject

--- ruby/spec/name_normalizer_spec.rb
+++ ruby/spec/name_normalizer_spec.rb
@@ -1,41 +1,39 @@
 require "./lib/name_normalizer"
 
 describe NameNormalizer do
-  let(:normalizer) { NameNormalizer.new }
-
   it "returns the empty string when passed an empty string" do
-    expect(normalizer.normalize "").to eq ""
+    expect(subject.normalize "").to eq ""
   end
 
   xit "returns a monomym when passed one" do
-    expect(normalizer.normalize "Plato").to eq "Plato"
+    expect(subject.normalize "Plato").to eq "Plato"
   end
 
   xit "swaps first and last names" do
-    expect(normalizer.normalize "Haruki Murakami").to eq "Murakami, Haruki"
+    expect(subject.normalize "Haruki Murakami").to eq "Murakami, Haruki"
   end
 
   xit "trims leading and trailing whitespace" do
-    expect(normalizer.normalize "   Big Boi    ").to eq "Boi, Big"
+    expect(subject.normalize "   Big Boi    ").to eq "Boi, Big"
   end
 
   xit "initializes middle name" do
-    expect(normalizer.normalize "Henry David Thoreau").to eq "Thoreau, Henry D."
+    expect(subject.normalize "Henry David Thoreau").to eq "Thoreau, Henry D."
   end
 
   xit "does not initialize one-letter middle name" do
-    expect(normalizer.normalize "Harry S Truman").to eq "Truman, Harry S"
+    expect(subject.normalize "Harry S Truman").to eq "Truman, Harry S"
   end
 
   xit "initializes each of multiple middle names" do
-    expect(normalizer.normalize "Julia Scarlett Elizabeth Louis-Dreyfus").to eq "Louis-Dreyfus, Julia S. E."
+    expect(subject.normalize "Julia Scarlett Elizabeth Louis-Dreyfus").to eq "Louis-Dreyfus, Julia S. E."
   end
 
   xit "appends suffixes to end" do
-    expect(normalizer.normalize "Martin Luther King, Jr.").to eq "King, Martin L., Jr."
+    expect(subject.normalize "Martin Luther King, Jr.").to eq "King, Martin L., Jr."
   end
 
   xit "throws when name contains too many commas" do
-    expect { normalizer.normalize "Thurston, Howell, III" }.to raise_error(ArgumentError)
+    expect { subject.normalize "Thurston, Howell, III" }.to raise_error(ArgumentError)
   end
 end
