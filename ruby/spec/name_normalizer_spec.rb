972d5555d9ecdb05ba9371021aee93163676cdd1
Jeff L <jeff@langrsoft.com>
2020-12-23 13:55:02 -0700

    righted ruby org

--- ruby/spec/name_normalizer_spec.rb
+++ ruby/spec/name_normalizer_spec.rb
@@ -1,4 +1,4 @@
-require "../lib/name_normalizer"
+require "./lib/name_normalizer"
 
 describe NameNormalizer do
   let(:normalizer) { NameNormalizer.new }
@@ -7,35 +7,35 @@ describe NameNormalizer do
     expect(normalizer.normalize "").to eq ""
   end
 
-  it "returns a monomym when passed one" do
+  xit "returns a monomym when passed one" do
     expect(normalizer.normalize "Plato").to eq "Plato"
   end
 
-  it "swaps first and last names" do
+  xit "swaps first and last names" do
     expect(normalizer.normalize "Haruki Murakami").to eq "Murakami, Haruki"
   end
 
-  it "trims leading and trailing whitespace" do
+  xit "trims leading and trailing whitespace" do
     expect(normalizer.normalize "   Big Boi    ").to eq "Boi, Big"
   end
 
-  it "initializes middle name" do
+  xit "initializes middle name" do
     expect(normalizer.normalize "Henry David Thoreau").to eq "Thoreau, Henry D."
   end
 
-  it "does not initialize one-letter middle name" do
+  xit "does not initialize one-letter middle name" do
     expect(normalizer.normalize "Harry S Truman").to eq "Truman, Harry S"
   end
 
-  it "initializes each of multiple middle names" do
+  xit "initializes each of multiple middle names" do
     expect(normalizer.normalize "Julia Scarlett Elizabeth Louis-Dreyfus").to eq "Louis-Dreyfus, Julia S. E."
   end
 
-  it "appends suffixes to end" do
+  xit "appends suffixes to end" do
     expect(normalizer.normalize "Martin Luther King, Jr.").to eq "King, Martin L., Jr."
   end
 
-  it "throws when name contains too many commas" do
+  xit "throws when name contains too many commas" do
     expect { normalizer.normalize "Thurston, Howell, III" }.to raise_error(ArgumentError)
   end
 end
