9cd062972af1466920f9d127d8b126b4b1b34b77
Jeff L <jeff@langrsoft.com>
2020-12-23 12:27:42 -0700

    JS solution

--- js/src/name-normalizer.js
+++ js/src/name-normalizer.js
@@ -1,3 +1,31 @@
 export const normalize = name => {
-  return "wha?";
+  throwWhenNameContainsTooManyCommas(name);
+  const parts = suffixless(name).trim().split(' ');
+  if (parts.length === 1) return name;
+  return `${lastName(parts)}, ${firstName(parts)}${middleInitials(parts)}${suffix(name)}`;
 }
+
+const throwWhenNameContainsTooManyCommas = name => {
+  if (commaCount(name) > 1)
+    throw new TypeError();
+}
+
+const middleInitials = parts => {
+  if (parts.length <= 2) return "";
+  return parts.slice(1, -1).map(initial).join('');
+}
+
+const suffixless = name => name.split(',')[0];
+
+const suffix = name => {
+  const suffix = name.split(',')[1];
+  return suffix ? `,${suffix}` : '';
+}
+
+const initial = name => ` ${name[0]}${name.length === 1 ? '': '.'}`;
+
+const lastName = parts => parts[parts.length - 1];
+
+const firstName = parts => parts[0];
+
+const commaCount = name => (name.match(/,/g) || []).length;
