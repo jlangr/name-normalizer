b3c5507b611501bbd7e69a3bce832faa6a39ede6
Jeff L <jeff@langrsoft.com>
2021-02-15 08:27:44 -0700

    remove completed solution for elixir from main

--- elixir/lib/name_normalizer.ex
+++ elixir/lib/name_normalizer.ex
@@ -1,35 +1,5 @@
 defmodule NameNormalizer do
   def normalize(name) do
-    name |> String.split(", ") |> normalize_name_and_maybe_suffix()
+    "hm?"
   end
-
-  defp normalize_name_and_maybe_suffix([name, suffix]) do
-    with {:ok, normalized_name} <- normalize_name_and_maybe_suffix([name]) do
-      {:ok, "#{normalized_name}, #{suffix}"}
-    end
-  end
-
-  defp normalize_name_and_maybe_suffix([name]) do
-    {:ok, name |> String.split() |> handle_parts()}
-  end
-
-  defp normalize_name_and_maybe_suffix(_) do
-    {:error, "Too many commas"}
-  end
-
-  defp handle_parts([]), do: ""
-  defp handle_parts([single_name]), do: single_name
-  defp handle_parts([first_name, last_name]), do: "#{last_name}, #{first_name}"
-
-  defp handle_parts([first_name | other_names]) do
-    {middle_names, last_name} = Enum.split(other_names, -1)
-    Enum.join(["#{last_name},", first_name, initials(middle_names)], " ")
-  end
-
-  defp initials(middle_names) do
-    middle_names |> Enum.map(&initial/1) |> Enum.join(" ")
-  end
-
-  defp initial(<<_>> = name), do: String.at(name, 0)
-  defp initial(name), do: "#{String.at(name, 0)}."
 end
--- elixir/test/name_normalizer_test.exs
+++ elixir/test/name_normalizer_test.exs
@@ -5,35 +5,43 @@ defmodule NameNameNormalizerTest do
     assert NameNormalizer.normalize("") == {:ok, ""}
   end
 
+  @tag :pending
   test "returns single word name" do
     assert NameNormalizer.normalize("Plato") == {:ok, "Plato"}
   end
 
+  @tag :pending
   test "swaps first and last names" do
     assert NameNormalizer.normalize("Haruki Murakami") == {:ok, "Murakami, Haruki"}
   end
 
+  @tag :pending
   test "trims leading and trailing whitespace" do
     assert NameNormalizer.normalize("  Big Boi   ") == {:ok, "Boi, Big"}
   end
 
+  @tag :pending
   test "initializes middle name" do
     assert NameNormalizer.normalize("Henry David Thoreau") == {:ok, "Thoreau, Henry D."}
   end
 
+  @tag :pending
   test "does not initialize one letter middle name" do
     assert NameNormalizer.normalize("Harry S Truman") == {:ok, "Truman, Harry S"}
   end
 
+  @tag :pending
   test "initializes each of multiple middle names" do
     assert NameNormalizer.normalize("Julia Scarlett Elizabeth Louis-Dreyfus") ==
              {:ok, "Louis-Dreyfus, Julia S. E."}
   end
 
+  @tag :pending
   test "appends suffixes to end" do
     assert NameNormalizer.normalize("Martin Luther King, Jr.") == {:ok, "King, Martin L., Jr."}
   end
 
+  @tag :pending
   test "returns an error when name contains two commas" do
     assert NameNormalizer.normalize("Thurston, Howell, III") == {:error, "Too many commas"}
   end
