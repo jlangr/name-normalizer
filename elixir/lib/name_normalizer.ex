1ba04a6c747d6db930bcc467af100868b2c8b3f7
Jeff L <jeff@langrsoft.com>
2021-02-15 08:33:02 -0700

    small tweaking on Elixir solution

--- elixir/lib/name_normalizer.ex
+++ elixir/lib/name_normalizer.ex
@@ -1,35 +1,28 @@
 defmodule NameNormalizer do
-  def normalize(name) do
-    name |> String.split(", ") |> normalize_name_and_maybe_suffix()
-  end
+  def normalize(name), do:
+    name |> String.split(", ") |> normalize_name_and_maybe_suffix
 
-  defp normalize_name_and_maybe_suffix([name, suffix]) do
-    with {:ok, normalized_name} <- normalize_name_and_maybe_suffix([name]) do
+  defp normalize_name_and_maybe_suffix([name, suffix]), do:
+    (with {:ok, normalized_name} <- normalize_name_and_maybe_suffix([name]) do
       {:ok, "#{normalized_name}, #{suffix}"}
-    end
-  end
+    end)
 
-  defp normalize_name_and_maybe_suffix([name]) do
-    {:ok, name |> String.split() |> handle_parts()}
-  end
-
-  defp normalize_name_and_maybe_suffix(_) do
+  defp normalize_name_and_maybe_suffix([name]), do:
+    {:ok, name |> String.split |> handle_parts }
+  defp normalize_name_and_maybe_suffix(_), do:
     {:error, "Too many commas"}
-  end
 
   defp handle_parts([]), do: ""
   defp handle_parts([single_name]), do: single_name
   defp handle_parts([first_name, last_name]), do: "#{last_name}, #{first_name}"
-
   defp handle_parts([first_name | other_names]) do
     {middle_names, last_name} = Enum.split(other_names, -1)
     Enum.join(["#{last_name},", first_name, initials(middle_names)], " ")
   end
 
-  defp initials(middle_names) do
+  defp initials(middle_names), do:
     middle_names |> Enum.map(&initial/1) |> Enum.join(" ")
-  end
 
-  defp initial(<<_>> = name), do: String.at(name, 0)
-  defp initial(name), do: "#{String.at(name, 0)}."
+  defp initial(<<_>> = name), do: String.first(name)
+  defp initial(name), do: "#{String.first(name)}."
 end
