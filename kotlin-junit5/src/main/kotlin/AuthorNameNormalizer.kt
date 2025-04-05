b83d50cdc81aae4153daf38facd068d750b3cd0e
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2025-04-05 05:52:46 -0400

    . r lift return out of if

--- kotlin-junit5/src/main/kotlin/AuthorNameNormalizer.kt
+++ kotlin-junit5/src/main/kotlin/AuthorNameNormalizer.kt
@@ -1,13 +1,19 @@
 class AuthorNameNormalizer {
     fun normalize(rawName: String): String {
-        if (rawName.contains(' ')) {
-            val parts = rawName.split(" ").filter { it.isNotBlank() }
-            return if (parts.size == 3) {
-                "${parts[2]}, ${parts[0]} ${parts[1].uppercase().first()}."
-            } else {
-                "${parts[1]}, ${parts[0]}"
-            }
-        }
-        return rawName
+        return if (isMultipart(rawName)) {
+            normalizeMultipart(rawName)
+        } else
+            rawName
     }
+
+    private fun normalizeMultipart(rawName: String): String {
+        val parts = rawName.split(" ").filter { it.isNotBlank() }
+        return if (parts.size == 3) {
+            "${parts[2]}, ${parts[0]} ${parts[1].uppercase().first()}."
+        } else {
+            "${parts[1]}, ${parts[0]}"
+        }
+    }
+
+    private fun isMultipart(rawName: String) = rawName.contains(' ')
 }
