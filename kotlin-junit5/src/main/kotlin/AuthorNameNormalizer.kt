2dcf2e487a1316e7e373b75e0eee1e23a0dc6187
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2025-04-05 05:32:05 -0400

    . F pass swap first and last names test (GREEN)

--- kotlin-junit5/src/main/kotlin/AuthorNameNormalizer.kt
+++ kotlin-junit5/src/main/kotlin/AuthorNameNormalizer.kt
@@ -1,5 +1,9 @@
 class AuthorNameNormalizer {
     fun normalize(s: String): String {
+        if (s.contains(' ')) {
+            val parts = s.split(" ")
+            return "${parts[1]}, ${parts[0]}"
+        }
         return s
     }
 }
