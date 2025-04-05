05aeb09a7c4243493fa3f5ea7644e19a2138736d
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2025-04-05 05:41:51 -0400

    . F pass strip leading and trailing space test (GREEN)

--- kotlin-junit5/src/main/kotlin/AuthorNameNormalizer.kt
+++ kotlin-junit5/src/main/kotlin/AuthorNameNormalizer.kt
@@ -1,7 +1,7 @@
 class AuthorNameNormalizer {
     fun normalize(rawName: String): String {
         if (rawName.contains(' ')) {
-            val parts = rawName.split(" ")
+            val parts = rawName.split(" ").filter { it.isNotBlank() }
             return "${parts[1]}, ${parts[0]}"
         }
         return rawName
