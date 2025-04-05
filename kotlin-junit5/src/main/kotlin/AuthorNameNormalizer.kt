f324d9c1903510c7cc7fb4c74a4b76ff5fec4b37
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2025-04-05 05:47:58 -0400

    . r lift return out of if per IDE suggestion (REFACTOR)

--- kotlin-junit5/src/main/kotlin/AuthorNameNormalizer.kt
+++ kotlin-junit5/src/main/kotlin/AuthorNameNormalizer.kt
@@ -2,10 +2,10 @@ class AuthorNameNormalizer {
     fun normalize(rawName: String): String {
         if (rawName.contains(' ')) {
             val parts = rawName.split(" ").filter { it.isNotBlank() }
-            if (parts.size == 3) {
-                return "${parts[2]}, ${parts[0]} ${parts[1].uppercase().first()}."
+            return if (parts.size == 3) {
+                "${parts[2]}, ${parts[0]} ${parts[1].uppercase().first()}."
             } else {
-                return "${parts[1]}, ${parts[0]}"
+                "${parts[1]}, ${parts[0]}"
             }
         }
         return rawName
