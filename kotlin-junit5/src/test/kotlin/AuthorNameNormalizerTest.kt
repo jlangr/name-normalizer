cf425e1b641caf5fb5f7c76e5d5708aaa7c2cac6
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2025-05-20 12:56:51 -0400

    Add kotlin-junit5 solution starting point

---
+++ kotlin-junit5/src/test/kotlin/AuthorNameNormalizerTest.kt
@@ -0,0 +1,71 @@
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+
+class AuthorNameNormalizerTest {
+    private val normalizer = AuthorNameNormalizer()
+
+    @Disabled
+    @Test
+    fun `returns empty string when empty`() {
+        assertThat(normalizer.normalize(""))
+            .isEqualTo("")
+    }
+
+    @Disabled
+    @Test
+    fun `returns single word name`() {
+        assertThat(normalizer.normalize("Plato"))
+            .isEqualTo("Plato")
+    }
+
+    @Disabled
+    @Test
+    fun `swaps first and last names`() {
+        assertThat(normalizer.normalize("Haruki Murakami"))
+            .isEqualTo("Murakami, Haruki")
+    }
+
+    @Disabled
+    @Test
+    fun `trims leading and trailing whitespace`() {
+        assertThat(normalizer.normalize("  Big Boi   "))
+            .isEqualTo("Boi, Big")
+    }
+
+    @Disabled
+    @Test
+    fun `initializes middle name`() {
+        assertThat(normalizer.normalize("Henry David Thoreau"))
+            .isEqualTo("Thoreau, Henry D.")
+    }
+
+    @Disabled
+    @Test
+    fun `does not initialize one letter middle name`() {
+        assertThat(normalizer.normalize("Harry S Truman"))
+            .isEqualTo("Truman, Harry S")
+    }
+
+    @Disabled
+    @Test
+    fun `initializes each of multiple middle names`() {
+        assertThat(normalizer.normalize("Julia Scarlett Elizabeth Louis-Dreyfus"))
+            .isEqualTo("Louis-Dreyfus, Julia S. E.")
+    }
+
+    @Disabled
+    @Test
+    fun `appends suffixes to end`() {
+        assertThat(normalizer.normalize("Martin Luther King, Jr."))
+            .isEqualTo("King, Martin L., Jr.")
+    }
+
+    @Disabled
+    @Test
+    fun `throws when name contains two commas`() {
+        assertThatExceptionOfType(IllegalArgumentException::class.java)
+            .isThrownBy { normalizer.normalize("Thurston, Howell, III") }
+            .withMessage("name contains two commas")
+    }
+}
